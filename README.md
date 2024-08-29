e5f6cf91da9edd442e3e315b0a46fadb5d050fe8
Adam Sawyer <30847837+adamjst@users.noreply.github.com>
2024-08-29 14:02:05 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,91 @@
+# Relevant Research's Replication Study of Harvard University's "Endless Nightmare" Report
+
+This repository is the result of Relevant Research's replication study of ["Endless Nightmare" (Ardalan et al., 2024)](https://phr.org/our-work/resources/endless-nightmare-solitary-confinement-in-us-immigration-detention/) published by students and faculty of the Harvard Immigration and Refugee Clinical Program (HIRCP) and Harvard Law School (HLS), members of the Peeler Immigration Lab (PIL) at Harvard Medical School (HMS), and Physicians for Human Rights (PHR).
+
+### Summary
+
+An interdisciplinary collaboration of lawyers, physicians, and social scientists from Harvard University published the report [“‘Endless Nightmare’: Torture and Inhuman Treatment in Solitary Confinement in U.S. Immigration Detention”]("https://phr.org/our-work/resources/endless-nightmare-solitary-confinement-in-us-immigration-detention/") in February 2024. 
+The report was based on data obtained through Freedom of Information Act (FOIA) requests sent to Immigration and Customs Enforcement (ICE) related to the use of solitary confinement in U.S. immigration detention.
+In keeping with best practices of transparent scholarship, the authors commendably made the underlying data for their project available online through a public repository on Harvard's Dataverse platform.
+To demonstrate Relevant Research's approach to data analysis, we used the publicly-available data to replicate the 'Endless Nightmare' study.
+Our process and findings are outlined briefly below, while all of our R code and replicated figures are available in this repository.
+
+### Method and Tools
+
+This replication study is limited in scope to the first part of the report, based specifically on a reproduction of the ICE solitary confinement data obtained from the Harvard team's FOIA requests.
+The researchers presented their analysis on pages one through eighteen of their report, with figures two through four and five through seven constructed from summaries of the ICE dataset.
+All in-text statistics, tables, and figures have been replicated here.
+This replication does not include an analysis of the in-person interviews of former detainees that make up the rest of the report. 
+Relevant Research's replication study was undertaken independently from the Harvard team.
+
+Relevant Research relied on the Harvard study’s metadata (available on Dataverse) and conducted the replication study using R, rather than Stata, the programming language used by the original researchers – yet we arrived at the same results.
+The method of analysis is described in further detail in the accompanying “how-to” document that covers how a general data science workflow can be applied when working with the specific opportunities and challenges that are presented by administrative data.
+
+The Harvard team's original repository for their report can be found [on Dataverse here](https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/AT7YFA) and their final report can be found [here](phr.org/our-work/resources/endless-nightmare-solitary-confinement-in-us-immigration-detention/).
+
+### Data 
+
+The dataset under analysis consists of 17,000 case-by-case records of solitary confinement stays between 2018 and 2023. ICE redacted five of the 23 fields under FOIA exemptions related to personal privacy (b)(6), law enforcement (b)(7)(e), or both. After removing the approximately 3,000 cases in which the release date is not entered, the number of stays over that time period aggregates to over 14,000 stays in solitary detention.
+
+The research team appears to have done some minimal preliminary processing of the dataset before uploading it to the Harvard Dataverse repository. This included removing the header rows used by government agencies for FOIA response tracking and modifying the date fields to the preferred YYYY-MM-DD format.
+
+### Output
+
+Figures 2-4 and 6-7 can be seen below. Figure 5 was a replication of an ICE graph that did not require further reproduction. Underlying datasets are linked.
+For in-text citations of data, the script `replication_code.r` contains the commented code to produce each statistic and the result.
+
+Fig. 2
+<img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/orig_fig2.png width="310"><img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/rep_fig2.png width="300">
+Data [here](https://github.com/Research-Basecamp/white-paper_replication/tree/main/figuredata)
+
+Fig. 3
+<img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/orig_fig3.png width="317"><img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/rep_fig3.png width="300">
+Data [here](https://github.com/Research-Basecamp/white-paper_replication/tree/main/figuredata)
+
+Fig. 4
+<img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/orig_fig4.png width="325"><img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/rep_fig4.png width="300">
+Data [here](https://github.com/Research-Basecamp/white-paper_replication/tree/main/figuredata)
+
+Fig. 6
+<img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/orig_fig6.png width="326"><img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/rep_fig6.png width="350">
+Data [here](https://github.com/Research-Basecamp/white-paper_replication/tree/main/figuredata)
+
+Fig. 7
+<img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/orig_fig7.png width="300"><img src=https://github.com/Research-Basecamp/white-paper_replication/blob/main/figures/rep_fig7.png width="306">
+Data [here](https://github.com/Research-Basecamp/white-paper_replication/tree/main/figuredata)
+
+### Results
+The vast majority of calculations using the FOIA case-by-case records are correct. 
+See the replication file `replication_code.r`.
+As seen above, graphics 2, 3, 4, 6, and 7 were successfully reproduced.
+
+This replication identified three minor discrepancies, which illustrate Relevant Research's attention to detail.
+Two of the three discrepancies are differences of a single whole number.
+The third discrepancy is a percentage calculation that is two percentage points off in Relevant Research's code than the researchers. 
+The three minor discrepancies are explained below.
+
+#### Page 14
+
+- **Original Text**: "682 solitary confinement placements lasted at least 90 days, while *42* lasted over a year."
+- **RR's Replication**: A total of *43* solitary confinement placements lasted over one year.
+
+#### Page 17
+
+- **Original Text**: "Among the *8,788* records for this period where ICE’s SRMS reported the mental health status of immigrants in solitary confinement..."
+- **RR's Replication**: There were *8,787* records which met those criteria.
+
+#### Page 17
+
+- **Original Text**: "The percentage of immigrants with mental health conditions placed in solitary confinement jumped from *35* percent in 2019 to 56 percent in 2023."
+- **RR's Replication**: The percentage of immigrants with mental illness in 2019 came to *33.3* percent.
+
+To reiterate, none of the discrepancies found here should assume that the research team committed errors in calculation.
+This replication merely points out the minimal situations in which the calculations do not seem to align between the researchers processing in Stata and our analysis in R.
+If any of the discrepancies do in fact rise to the level of error, it does not impact the findings of the Report.
+Solitary confinement in ICE detention remains a subject deserving of further scrutiny, and we applaud the research team for their persistence in documenting abuses and making their data, methods, and findings public.
+
+### Conclusion
+
+Relevant Research's replication study affirms the quality of the Harvard team's methodology and findings (we note only minimal discrepancies) and we make our results evident below.
+We commend the interdisciplinary team for publishing structured descriptions of work in public repositories and encourage more researchers to do the same.
+For questions or inquiries about how Relevant Research can provide data discovery (including through FOIA requests), data analysis, replication studies, data visualization, and data write-up, please email us at info@relevant-research.com.
---
+++ replication_code.r
@@ -0,0 +1,417 @@
+# Replication of pages 9-18 of the "Endless Nightmare" Report
+# Published 2024-02-06
+# Accessed at https://phr.org/our-work/resources/endless-nightmare-solitary-confinement-in-us-immigration-detention/
+library(tidyverse)
+library(janitor)
+library(readxl)
+library(stringi)
+library(openxlsx)
+
+# Read in Segregation Review Management System (SRMS) table received by researchers via FOIA request
+df <- read_excel('./SRMS spreadsheet 9.1.2018 - 9.1.2023 Redacted.xlsx')
+
+# Read in ICE data summarized from quarterly reports
+ice <- read_excel('./2022_2023_ICE_quarterly_confinement_vulnerable.xlsx', sheet = 5)
+
+## replicate table and graphics 2 and 3 from "Endless Nightmare" report
+
+fig2 <- df |>
+  filter(`Tracking Number` != "") |>#Filter out cases that have no tracking number 
+  filter(!is.na(`Release Date`)) |> #Filter out cases with no release date
+  mutate(`Mental Illness` = ifelse(`Mental Illness` == "Yes", #Standardize Mental Illness field
+                                   "Mental Illness", `Mental Illness`))|>
+  mutate(Facility = ifelse(Facility == "WEBB COUNTY DETENTION CENTER (TX)", #Standardize facility field
+                           "WEBB COUNTY DETENTION CENTER (CCA) (TX)", Facility)) |>
+  filter(`Detainee Request Segregation` == "Facility-Initiated")
+
+## Add column indicating source of ICE data
+ice_nums <- ice |>
+  mutate(source = "ICE_public")
+
+
+# add month-year
+fig2 <- fig2 |>
+  mutate(placement_year = year(as.Date(`Placement Date`)),
+         placement_fyq = quarter(as.Date(`Placement Date`), type="year.quarter",fiscal_start = 10))
+
+#subset of vulnerable placements. Replication here does not create separate bin column
+fig2_vuln <- fig2 |>
+  mutate(vulnerable = case_when(`Mental Illness` == "Mental Illness" ~ 1,
+                                `Mental Illness` == "Serious Mental Illness" ~ 1,
+                                `Serious Medical Illness` == "Yes" ~ 1,
+                                `Serious Disability` == "Yes" ~ 1,
+                                `Placement Reason` == "Protective Custody: Lesbian, Gay, Bisexual, Transgender (LGBT)" ~ 1,
+                                `Placement Reason` == "Protective Custody: Victim of Sexual Assault" ~ 1,
+                                `Placement Reason` == "Hunger Strike" ~ 1,
+                                `Placement Reason` == "Suicide Risk Placement" ~ 1,
+                                `Suicide Risk?` == "Suicide Risk"~1,
+                                .default = 0)) |>
+  filter(vulnerable==1)
+
+#summarize foia dataset by quarter and filter only fy2022-fy2023q3
+all_vuln_fyq <- fig2_vuln |>
+  group_by(placement_fyq)|>
+  summarize(Placement_FOIA = n(),
+            Length_FOIA = mean(`Length of Stay`)) |>
+  filter(placement_fyq >= 2022.1)|>
+  mutate(placement_fyq = case_when(placement_fyq == 2022.1 ~ "2022q1", ## Change numeric to character
+                                   placement_fyq == 2022.2 ~ "2022q2",
+                                   placement_fyq == 2022.3 ~ "2022q3",
+                                   placement_fyq == 2022.4 ~ "2022q4",
+                                   placement_fyq == 2023.1 ~ "2023q1",
+                                   placement_fyq == 2023.2 ~ "2023q2",
+                                   placement_fyq == 2023.3 ~ "2023q3",
+                                   placement_fyq == 2023.4 ~ "2023q4"),
+         source = "FOIA")
+
+
+#Merge FOIA and ICE-related data
+combined <- all_vuln_fyq |> 
+  left_join(ice_nums, by=c("placement_fyq"="PlacementQ")) |>
+  filter(!is.na(Placement_ICE)) 
+
+#Write
+#write.csv(combined, "rep_fig2-3.csv", row.names = FALSE)
+
+
+#####Visualization
+
+#Convert placement number data to long format
+fig2_data <- combined |>
+  select(c(placement_fyq, Placement_FOIA, Placement_ICE)) |>
+  pivot_longer(!placement_fyq, names_to = "Source", values_to = "Placements")
+
+#Factor Source to bring proper order to visualization
+fig2_data$Source <- factor(fig2_data$Source, levels = c("Placement_ICE", "Placement_FOIA"))
+
+write.csv(fig2_data, "fig2_data.csv", row.names = FALSE)
+#Convert length of solitary confinement data to long format
+fig3_data <- combined |>
+  select(c(placement_fyq, Length_FOIA, Length_ICE))|>
+  pivot_longer(!placement_fyq, names_to = "Source", values_to = "Mean Length")
+
+#Factor Source to bring proper order to visualization
+fig3_data$Source <- factor(fig3_data$Source, levels = c("Length_ICE", "Length_FOIA"))
+
+write.csv(fig3_data, "fig3_data.csv", row.names = FALSE)
+
+#Visualize placement numbers
+fig2_plot <- fig2_data |>
+  ggplot(aes(x=placement_fyq, y=Placements, group = Source, color = Source))+
+  geom_line() +
+  labs(title = "Replication of Figure 2 in the 'Endless Nightmare' Report",
+       subtitle = "Number of Solitary Confinement Placements for Immigrants with Vulnerabilities",
+       x = "",
+       y = "")+
+  scale_color_manual(labels = c("ICE Quarterly Placements", "SRMS FOIA Placements"), 
+                     values = c("red", "orange"))+
+  scale_y_continuous(limits = c(100, 425))+
+  theme_classic() +
+  theme(legend.title= element_blank())
+fig2_plot
+
+ggsave('rep_fig2.png')
+
+#Visualize length of confinement average
+fig3_plot <- fig3_data |>
+  ggplot(aes(x=placement_fyq, y=`Mean Length`, group = Source, color = Source))+
+  geom_line() +
+  labs(title = "Replication of Figure 3 in the 'Endless Nightmare' Report",
+       subtitle = "Length of Solitary Confinement for Immigrants with Vulnerabilities",
+       x = "",
+       y = "Days")+
+  
+  scale_color_manual(labels = c("ICE Quarterly Placements", "SRMS FOIA Placements"), 
+                     values = c("red", "orange"))+
+  scale_y_continuous(limits = c(5, 22))+
+  theme_classic()+
+  theme(legend.title= element_blank())
+fig3_plot
+
+ggsave('rep_fig3.png')
+
+# Replicating page 16-18 from "Eternal Nightmare" and graphics 4 and 6
+# start over with original dataset and clean variables
+df <- df |>
+  filter(`Tracking Number` != "") |>#Filter out cases that have no tracking number 
+  mutate(`Mental Illness` = ifelse(`Mental Illness` == "Yes", #Standardize Mental Illness field
+                                   "Mental Illness", `Mental Illness`))|>
+  mutate(Facility = ifelse(Facility == "WEBB COUNTY DETENTION CENTER (TX)", #Standardize facility field
+                           "WEBB COUNTY DETENTION CENTER (CCA) (TX)", Facility))
+
+#find mean/median length of stay. Pg. 14: 
+# "These placements lasted 27 days on average, well in excess of the 15-day period 
+# "that constitutes torture, as defined by the Special Rapporteur on Torture. 
+# Indeed, with a median length of confinement of 15 days"
+summarize_length <- df |>
+  filter(!is.na(`Release Date`))|> #Remove any instances where there are no release dates
+  summarize(count = n(),
+            mean_length = mean(`Length of Stay`), #26.6 days
+            median_length = median(`Length of Stay`)) #15 days
+
+#Pg. 14.
+length_90 <-df |> #Determine number of instances when a detainee had a mental illness and had a stay > 90 days
+  filter(`Tracking Number` != "") |>#Filter out cases that have no tracking number 
+  filter(!is.na(`Release Date`))|>
+  filter(`Length of Stay` >= 90) |> #682 solitary confinement placements > 90 days
+  group_by(`Mental Illness`)|>
+  count() # (140 mental illness + 53 serious mental illness) / 682 = 28.3% (almost 30 percent had mental health condition)
+
+#Pg. 14.
+length_365 <-df |> #Determine number of instances when a detainee had a mental illness and had a stay > 365 days
+  filter(`Tracking Number` != "") |>#Filter out cases that have no tracking number 
+  filter(!is.na(`Release Date`))|>
+  filter(`Length of Stay` >= 365) |> #43 detainees (Discrepancy. 42 reported in text)
+  group_by(`Mental Illness`)|>
+  count() # (8 mental illness + 2 serious mental illness) / 43 = 23.3% (~almost 25 percent had mental health condition)
+
+
+## Sort facilities by longest length of stay and average length of stay
+longest_stays <- df |>
+  filter(!is.na(`Release Date`))|>
+  group_by(Facility, `Length of Stay`)|>
+  count() |>
+  arrange(desc(`Length of Stay`)) #Denver Contract - 727 days; Otay Mesa 759/567 Days, etc...
+
+## Average Stay by Facility
+average_staysxfacility <- df |>
+  filter(!is.na(`Release Date`))|>
+  group_by(Facility) |>
+  summarise(mean_stay = mean(`Length of Stay`)) |>
+  arrange(desc(mean_stay)) #Northwest ICE average length is 55 days (Tacoma); Denver-52 days.
+
+
+## Year by year placements
+
+#month-year
+time <- df |>
+  mutate(placement_year = year(as.Date(`Placement Date`)),
+         placement_month = month(as.Date(`Placement Date`)),
+         placement_fyq = quarter(as.Date(`Placement Date`), type="year.quarter",fiscal_start = 10),
+         release_year = year(as.Date(`Release Date`)))
+
+yearly <- time |>
+  group_by(placement_year)|>
+  count() #"As of September 2023, there were already 2,301 reported placements." 
+          # projection (2301/8 months)*12 = 3452 placements
+          # Peak number of placements in 2020
+ 
+monthly <- time |>
+  group_by(placement_year, placement_month)|>
+  count() # Lowest number of placements in mid/late-2021
+
+release_date <- time |>
+  filter(!is.na(`Release Date`))|>
+  group_by(release_year)|>
+  summarise(mean_length = mean(`Length of Stay`))   # Average length above 15 days each of past five years
+                                                    # Average was 23 days in 2023 as of Sept 2023
+
+no_release_date <- time |>
+  filter(is.na(`Release Date`))|>
+  group_by(release_year)|>
+  summarise(mean_length = mean(`Length of Stay`)) 
+# For those without a release date, average length is 65 days 
+
+## Pg. 15
+###Proportion of solitary per 10k detained
+det <- read_excel("2019_2023_daily_detention_confinement.xlsx", sheet = 'combined')
+
+##Plot graphic 4
+detplot <- det |>
+  ggplot(aes(x=YearMonth, y=confinement_prop))+
+  geom_line(color = "orange") +
+  labs(title = "Replication of Figure 4 in the 'Endless Nightmare' Report",
+       subtitle = "Number of Immigrants Held in Solitary Confinement out of Total Population in Detention",
+       x="",
+       y="")+
+  scale_y_continuous(limits = c(0,200))+
+  theme_classic()
+detplot
+
+ggsave("rep_fig4.png")
+
+# Page 16
+
+#clean variables
+df <- df |>
+  filter(`Tracking Number` != "") |>#Filter out cases that have no tracking number 
+  mutate(`Mental Illness` = ifelse(`Mental Illness` == "Yes", #Standardize Mental Illness field
+                                   "Mental Illness", `Mental Illness`))|>
+  mutate(Facility = ifelse(Facility == "WEBB COUNTY DETENTION CENTER (TX)", #Standardize facility field
+                           "WEBB COUNTY DETENTION CENTER (CCA) (TX)", Facility))
+
+### Graphic 6
+
+time <- df |>
+  filter(!is.na(`Release Date`))|>
+  mutate(placement_year = year(as.Date(`Placement Date`)),
+         placement_fyq = quarter(as.Date(`Placement Date`), type="year.quarter",fiscal_start = 10),
+         release_year = year(as.Date(`Release Date`)))
+
+avg_length <- time |>
+  group_by(release_year) |>
+  summarize(mean_length = mean(`Length of Stay`)) |>
+  filter(release_year >= 2019) |>
+  mutate(UN_Def = 15) |>
+  pivot_longer(!release_year, names_to = "Length of Stay", values_to = "Days")
+
+#write.csv(avg_length, "fig6_data.csv", row.names = FALSE)
+
+### Graphic 6
+fig6 <- avg_length |>
+  ggplot(aes(x=release_year, y=Days))+
+  geom_line(aes(color = `Length of Stay`), size=2)+
+  scale_color_manual(values = c("red", "purple"),
+                     labels=c("Average Number of Days \n in Solitary Confinement", 
+                              "UN Definition \n of Torture"))+
+  labs(title = "Replication of Figure 6 in the 'Endless Nightmare'' Report",
+       subtitle = "Average Number of Days in Solitary Confinement",
+       x = "",
+       y = "")+
+  scale_y_continuous(limits = c(0, 35), breaks = seq(0,35,5))+
+  theme_classic()+
+  theme(legend.title = element_blank(),
+        legend.spacing.y = unit(1, 'cm'))+
+  guides(color = guide_legend(byrow = TRUE))
+fig6  
+
+ggsave("rep_fig6.png")
+
+# LGBT statistic
+lgbt <- df |>
+  filter(`Placement Reason` == "Protective Custody: Lesbian, Gay, Bisexual, Transgender (LGBT)")
+
+
+lgbt_fyq <- lgbt |>
+  mutate(placement_fyq = quarter(as.Date(`Placement Date`), type="year.quarter",fiscal_start = 10))|>
+  group_by(placement_fyq)|>
+  count() |>
+  adorn_totals(c('row')) # 62 detainees placed in confinement
+
+summary(lgbt$`Length of Stay`) ## Protective Custody for LGBT detainees: Mean 57 days; median 34; max 286 days
+
+##Pg 17
+df <- df |>
+  filter(`Tracking Number` != "") |>#Filter out cases that have no tracking number 
+  mutate(`Mental Illness` = ifelse(`Mental Illness` == "Yes", #Standardize Mental Illness field
+                                   "Mental Illness", `Mental Illness`))|>
+  mutate(Facility = ifelse(Facility == "WEBB COUNTY DETENTION CENTER (TX)", #Standardize facility field
+                           "WEBB COUNTY DETENTION CENTER (CCA) (TX)", Facility))
+
+mental_illness <- df |>  # 8,787 records (Discrepancy. Off by one record); 2882+642/8787=40.1%
+  group_by(`Mental Illness`)|> 
+  filter(!is.na(`Mental Illness`))|>
+  count() |>
+  adorn_totals("row")
+# Over 40 percent had documented mental health conditions
+
+mental_illnes_nacount <- df |> #(5477/14264=38.4% NAs) 
+  group_by(`Mental Illness`)|> 
+  count() |>
+  adorn_totals("row")
+# "ICE reported immigrants' mental health in only 62 percent of total solitary confinement records.
+
+mental_year <- df |>
+  mutate(placement_year = year(as.Date(`Placement Date`)))|>
+  group_by(`Mental Illness`, placement_year) |>
+  count()
+
+mental_19 <- df |> ## 2019 : Mental Illness = 26.6% + Serious Illness = 6.7% == 33.3% (Discrepancy. Off by 2 percentage points)
+  mutate(placement_year = year(as.Date(`Placement Date`)))|> # "Jumped from 35 percent in 2019 to 56 percent in 2023"
+  filter(placement_year == "2019") |>
+  group_by(`Mental Illness`, placement_year) |>
+  count() |>
+  ungroup()|>
+  filter(!is.na(`Mental Illness`))|>
+  mutate(total = sum(n), 
+         pct = n/total)
+
+mental_23 <- df |> ## 2023 : Mental Illness = 41.0% + Serious Illness = 15.2% == 56.2%
+  mutate(placement_year = year(as.Date(`Placement Date`)))|>
+  filter(placement_year == "2023") |>
+  group_by(`Mental Illness`, placement_year) |>
+  count() |>
+  ungroup()|>
+  filter(!is.na(`Mental Illness`))|>
+  mutate(total = sum(n), 
+         pct = n/total)
+
+serious_mental_19 <- df |> ## 2019 : Serious Mental Illness is 20.0% of all with mental illness
+  mutate(placement_year = year(as.Date(`Placement Date`)))|>
+  filter(placement_year == "2019") |>
+  filter(`Mental Illness` == "Mental Illness" | `Mental Illness` == "Serious Mental Illness") |>
+  group_by(`Mental Illness`, placement_year) |>
+  count() |>
+  ungroup()|>
+  filter(!is.na(`Mental Illness`))|>
+  mutate(total = sum(n), 
+         pct = n/total)
+
+serious_mental_23 <- df |> ## 2023 : Mental Illness = 27.0%
+  mutate(placement_year = year(as.Date(`Placement Date`)))|>
+  filter(placement_year == "2023") |>
+  filter(`Mental Illness` == "Mental Illness" | `Mental Illness` == "Serious Mental Illness") |>
+  group_by(`Mental Illness`, placement_year) |>
+  count() |>
+  ungroup()|>
+  filter(!is.na(`Mental Illness`))|>
+  mutate(total = sum(n), 
+         pct = n/total)
+# 20 percent mental illnesses in 2019, 27 percent in 2023
+
+
+## Mental Illness length of stay
+mental_illness_length <- df |> ## Mental Illness 23.4 days; Serious # 34.4 days
+  mutate(placement_year = year(as.Date(`Placement Date`)))|>
+  group_by(`Mental Illness`)|>
+  summarize(mean_length = mean(`Length of Stay`))
+
+##Facilities length of stay by mental illness
+mental_illness_facility <- df |>
+  filter(`Mental Illness` == "Mental Illness" | `Mental Illness` == "Serious Mental Illness") |>
+  group_by(Facility)|>
+  summarize(mean_length = mean(`Length of Stay`))|>
+  arrange(desc(mean_length))
+#Richwood, Denver, Yuba, Otay Mesa, Henderson all ranged between 3 months (90 days) - 6 months (180 days)
+
+## Figure 7 on page 18
+mental_year <- df |>
+  mutate(release_year = year(as.Date(`Release Date`)))|>
+  mutate(mental_ill_bin = ifelse(`Mental Illness` == "Mental Illness" | 
+                                   `Mental Illness` == "Serious Mental Illness", "Mental Illness", "No Mental Illness"))|>
+  group_by(mental_ill_bin, release_year) |>
+  count()
+
+fig7 <- mental_year |>
+  filter(!is.na(mental_ill_bin))|>
+  filter(release_year > 2018)
+write.csv(fig7, "fig7_data.csv", row.names = FALSE)
+
+fig7 <- mental_year |>
+  filter(!is.na(mental_ill_bin))|>
+  filter(release_year > 2018)|>
+  ggplot(aes(x=release_year, y = n, fill=mental_ill_bin))+
+  geom_bar(position = "fill", stat="identity")+
+  scale_fill_manual(values = c("Mental Illness" = "#FB4F14", 
+                               "No Mental Illness" = "#002244"))+
+  scale_y_continuous(labels = scales::label_percent())+
+  labs(title = "Replication of Figure 7 in the 'Endless Nightmare' Report",
+       subtitle = "Percent in Solitary Confinement With Mental Illness",
+       x="",
+       y="Percent")+
+  annotate("text", x = 2019, y = 0.85, label = "35", color = 'white',
+           size = 10, parse = TRUE) +
+  annotate("text", x = 2020, y = 0.85, label = "33", color = 'white',
+           size = 10, parse = TRUE) +
+  annotate("text", x = 2021, y = 0.85, label = "44", color = 'white',
+           size = 10, parse = TRUE) +
+  annotate("text", x = 2022, y = 0.85, label = "55", color = 'white',
+           size = 10, parse = TRUE) +
+  annotate("text", x = 2023, y = 0.85, label = "56", color = 'white',
+           size = 10, parse = TRUE) +
+  theme_classic() +
+  theme(legend.title = element_blank())
+fig7
+
+ggsave("rep_fig7.png")
+
